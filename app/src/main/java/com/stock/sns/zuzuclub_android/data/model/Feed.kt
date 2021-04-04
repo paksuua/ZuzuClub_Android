b2b1cfe65ecd1c2e005ee3cf38d113a21d60547f
jklee <ksa671@naver.com>
2021-04-05 00:08:28 +0900

    UPDATE feed item xml

--- app/src/main/java/com/stock/sns/zuzuclub_android/data/model/Feed.kt
+++ app/src/main/java/com/stock/sns/zuzuclub_android/data/model/Feed.kt
@@ -3,7 +3,7 @@ package com.stock.sns.zuzuclub_android.data.model
 data class Feed(
     var profileImage:String,
     var nickname: String,
-    var updated: String, //백에서 들어오는 타입보고 변경
+    var updated: Int, //백에서 들어오는 타입보고 변경
     var emojiType: Int, //일단 없는거 0 여우 1, 사자 2, 토끼 3, 말 4
     var text: String,
     var reaction:Int,
@@ -14,7 +14,7 @@ fun getDefaultFeedList() = arrayListOf(
     Feed(
         profileImage = "http://placehold.it/120x120&text=image1",
         nickname = "방배동 살쾡이",
-        updated = "2",
+        updated = 2,
         emojiType = 3,
         text = "\$카카오 이제 들어간다~! ",
         reaction = 4,
@@ -23,7 +23,7 @@ fun getDefaultFeedList() = arrayListOf(
     Feed(
         profileImage = "http://placehold.it/120x120&text=image2",
         nickname = "방배동 살쾡이",
-        updated = "2",
+        updated = 2,
         emojiType = 3,
         text = "\$카카오 이제 들어간다~! ",
         reaction = 4,
@@ -32,7 +32,7 @@ fun getDefaultFeedList() = arrayListOf(
     Feed(
         profileImage = "http://placehold.it/120x120&text=image3",
         nickname = "방배동 살쾡이",
-        updated = "2",
+        updated = 2,
         emojiType = 3,
         text = "\$카카오 이제 들어간다~! ",
         reaction = 4,
@@ -41,7 +41,7 @@ fun getDefaultFeedList() = arrayListOf(
     Feed(
         profileImage = "http://placehold.it/120x120&text=image4",
         nickname = "방배동 살쾡이",
-        updated = "2",
+        updated = 2,
         emojiType = 3,
         text = "\$카카오 이제 들어간다~! ",
         reaction = 4,
@@ -50,7 +50,7 @@ fun getDefaultFeedList() = arrayListOf(
     Feed(
         profileImage = "http://placehold.it/120x120&text=image5",
         nickname = "방배동 살쾡이",
-        updated = "2",
+        updated = 2,
         emojiType = 3,
         text = "\$카카오 이제 들어간다~! ",
         reaction = 4,
@@ -59,7 +59,7 @@ fun getDefaultFeedList() = arrayListOf(
     Feed(
         profileImage = "http://placehold.it/120x120&text=image6",
         nickname = "방배동 살쾡이",
-        updated = "2",
+        updated = 2,
         emojiType = 3,
         text = "\$카카오 이제 들어간다~! ",
         reaction = 4,
@@ -68,7 +68,7 @@ fun getDefaultFeedList() = arrayListOf(
     Feed(
         profileImage = "http://placehold.it/120x120&text=image7",
         nickname = "방배동 살쾡이",
-        updated = "2",
+        updated = 2,
         emojiType = 3,
         text = "\$카카오 이제 들어간다~! ",
         reaction = 4,
@@ -77,7 +77,7 @@ fun getDefaultFeedList() = arrayListOf(
     Feed(
         profileImage = "http://placehold.it/120x120&text=image8",
         nickname = "방배동 살쾡이",
-        updated = "2",
+        updated = 2,
         emojiType = 3,
         text = "\$카카오 이제 들어간다~! ",
         reaction = 4,
@@ -86,7 +86,7 @@ fun getDefaultFeedList() = arrayListOf(
     Feed(
         profileImage = "http://placehold.it/120x120&text=image9",
         nickname = "방배동 살쾡이",
-        updated = "2",
+        updated = 2,
         emojiType = 3,
         text = "\$카카오 이제 들어간다~! ",
         reaction = 4,
@@ -95,7 +95,7 @@ fun getDefaultFeedList() = arrayListOf(
     Feed(
         profileImage = "http://placehold.it/120x120&text=image10",
         nickname = "방배동 살쾡이",
-        updated = "2",
+        updated = 2,
         emojiType = 3,
         text = "\$카카오 이제 들어간다~! ",
         reaction = 4,
