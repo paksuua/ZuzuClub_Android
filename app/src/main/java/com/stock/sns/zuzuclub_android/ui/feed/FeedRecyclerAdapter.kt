e691af668a41b0f4a77ae85835c0ab971bd69e8d
jklee <ksa671@naver.com>
2021-04-08 11:50:29 +0900

    ADD color,link in hashtag

--- app/src/main/java/com/stock/sns/zuzuclub_android/ui/feed/FeedRecyclerAdapter.kt
+++ app/src/main/java/com/stock/sns/zuzuclub_android/ui/feed/FeedRecyclerAdapter.kt
@@ -1,23 +1,44 @@
 package com.stock.sns.zuzuclub_android.ui.feed
 
+import android.app.Dialog
 import android.content.Context
+import android.graphics.Color
+import android.graphics.Typeface
 import android.graphics.drawable.ShapeDrawable
 import android.graphics.drawable.shapes.OvalShape
+import android.text.style.StyleSpan
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.LiveData
 import androidx.recyclerview.widget.RecyclerView
 import com.stock.sns.zuzuclub_android.BR
+import com.stock.sns.zuzuclub_android.R
 import com.stock.sns.zuzuclub_android.data.model.Feed
 import com.stock.sns.zuzuclub_android.databinding.ItemFeedBinding
+import com.volokh.danylo.hashtaghelper.HashTagHelper
+import io.github.armcha.autolink.MODE_CUSTOM
+import io.github.armcha.autolink.MODE_HASHTAG
 
 class FeedRecyclerAdapter(var itemlist: LiveData<ArrayList<Feed>>): RecyclerView.Adapter<FeedRecyclerAdapter.Holder>() {
     //lateinit var itemlist : ArrayList<Feed>
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         val binding = ItemFeedBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+//        val hashtagSimbol = '\$'
+//        var mTextHashTagHelper: HashTagHelper = HashTagHelper.Creator.create(
+//            Color.parseColor("#2d9cdb"),
+//            HashTagHelper.OnHashTagClickListener { Toast.makeText(parent.context, "click", Toast.LENGTH_LONG)
+//            },
+//            hashtagSimbol
+//        )
+//
+//        mTextHashTagHelper.handle(binding.iFeedTvText)
+
         return Holder(binding)
     }
 
@@ -32,6 +53,7 @@ class FeedRecyclerAdapter(var itemlist: LiveData<ArrayList<Feed>>): RecyclerView
 
     inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         lateinit var binding: ItemFeedBinding
+        val custom = MODE_CUSTOM("(?:^|\\s|\$|[.])\\$[\\p{L}0-9_]*")
 
         constructor(binding: ItemFeedBinding) : this(binding.root) {
             Log.d("ViewHolder", " create")
@@ -39,6 +61,12 @@ class FeedRecyclerAdapter(var itemlist: LiveData<ArrayList<Feed>>): RecyclerView
             binding.iFeedIvProfile.apply {
                 background = ShapeDrawable(OvalShape())
                 clipToOutline = true
+            }
+            binding.iFeedTvText.apply {
+                addAutoLinkMode(custom)
+                onAutoLinkClick { Log.e("feed adapter","tag click") }
+                customModeColor = Color.parseColor("#2d9cdb")
+                addSpan(custom, StyleSpan(Typeface.BOLD))
             }
 
         }
--- app/src/main/java/com/stock/sns/zuzuclub_android/ui/signup/TermsActivity.kt
+++ app/src/main/java/com/stock/sns/zuzuclub_android/ui/signup/TermsActivity.kt
@@ -2,6 +2,7 @@ package com.stock.sns.zuzuclub_android.ui.signup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.stock.sns.zuzuclub_android.R
 
 class TermsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
