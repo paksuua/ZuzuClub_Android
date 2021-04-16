4c10875cd813b2669083330be51d7ce59a7433fc
Soyeon Park <paksuua@naver.com>
2021-04-17 02:17:55 +0900

    UPDATE BottomNavigation 탐색 방법 변경

--- app/src/main/java/com/stock/sns/zuzuclub_android/ui/MainActivity.kt
+++ app/src/main/java/com/stock/sns/zuzuclub_android/ui/MainActivity.kt
@@ -3,37 +3,45 @@ package com.stock.sns.zuzuclub_android.ui
 import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.stock.sns.zuzuclub_android.R
 import com.stock.sns.zuzuclub_android.databinding.ActivityMainBinding
+import com.stock.sns.zuzuclub_android.ui.home.HomeFragment
+import com.stock.sns.zuzuclub_android.ui.post.PostFragment
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val fragmentPost by lazy { PostFragment() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        // system bar transparent
-        // iniTransparentSystemBar()
+        // initTransparentSystemBar()
 
         initNavigationBar()
     }
 
     private fun initNavigationBar() {
-        val navView: BottomNavigationView = findViewById(R.id.btnv_view)
-
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fcv_main) as NavHostFragment
         val navController = navHostFragment.navController
-        navView.setupWithNavController(navController)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.navigation_post) {
+                binding.btnvView.visibility = View.GONE
+            } else {
+                binding.btnvView.visibility = View.VISIBLE
+            }
+        }
+        binding.btnvView.setupWithNavController(navController)
         binding.btnvView.itemIconTintList = null
     }
 
-    private fun iniTransparentSystemBar() {
+    // 상단 시스템바를 투명하게
+    private fun initTransparentSystemBar() {
         window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
         binding.root.setPadding(0, 0, 0, statusBarHeight(this))
         window.statusBarColor = Color.TRANSPARENT
@@ -44,5 +52,17 @@ class MainActivity : AppCompatActivity() {
 
         return if (resourceId > 0) context.resources.getDimensionPixelSize(resourceId)
         else 0
+    }
+
+    override fun onBackPressed() {
+        if (fragmentPost.isVisible) {
+            if (fragmentPost.childFragmentManager.backStackEntryCount >= 1) {
+                fragmentPost.childFragmentManager.popBackStackImmediate()
+            } else {
+                super.onBackPressed()
+            }
+        } else {
+            super.onBackPressed()
+        }
     }
 }
--- app/src/main/java/com/stock/sns/zuzuclub_android/ui/post/PostFragment.kt
+++ app/src/main/java/com/stock/sns/zuzuclub_android/ui/post/PostFragment.kt
@@ -4,14 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
+import androidx.lifecycle.MutableLiveData
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.stock.sns.zuzuclub_android.R
+import com.stock.sns.zuzuclub_android.data.model.EmojiPackData
 import com.stock.sns.zuzuclub_android.databinding.FragmentPostBinding
 
 class PostFragment : Fragment() {
     private val viewModel by viewModels<PostViewModel>()
     private val binding by lazy { FragmentPostBinding.inflate(layoutInflater) }
+
+    var emojiPackData = MutableLiveData<ArrayList<EmojiPackData>>()
+    private lateinit var emojiPackAdapter: PostEmojiPackAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,12 +30,39 @@ class PostFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        viewModel.text.observe(
+        initUI()
+        return binding.root
+    }
+
+    private fun initUI() {
+        // 이모티콘 패키지
+        binding.rvPostEmojipack.layoutManager =
+            LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        viewModel.emojiPackData.observe(
             viewLifecycleOwner,
-            Observer {
-                binding.tvPost.text = it
+            {
+                emojiPackData.value = it
+                emojiPackAdapter = PostEmojiPackAdapter(emojiPackData)
+                binding.rvPostEmojipack.adapter = emojiPackAdapter
+                emojiPackAdapter.notifyDataSetChanged()
             }
         )
-        return binding.root
+
+        // 패키지별 이모티콘
+        binding.rvPostEmojis.layoutManager =
+            LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+
+        binding.edtPost.doOnTextChanged { text1, start, count, after ->
+            binding.ctvPostConfirm.isChecked = !text1.isNullOrBlank()
+        }
+
+        // 취소 or 완료
+        binding.tvPostBack.setOnClickListener { view ->
+            view.findNavController().navigate(R.id.action_to_navigation_home)
+        }
+
+        binding.ctvPostConfirm.setOnClickListener { view->
+            view.findNavController().navigate(R.id.action_to_navigation_home)
+        }
     }
 }
